b9d50675b59c7e247e8a3d781e4a0bfe91d415f1
vincevass <s2510769016@fhooe.at>
2026-01-03 23:10:02 +0100

    Initial commit of subtask1 and subtask2

---
+++ subtask1.py
@@ -0,0 +1,22 @@
+#INF1PR Subtask8.1
+def find_largest_square(n):
+    
+    m = 0
+    
+    while (m + 1)**2 <= n:
+        m = m + 1
+        
+    q = m**2
+    return q
+
+
+userInput = int(input("Enter a natural number n ≥ 0: "))
+    
+if userInput >= 0:
+        result = find_largest_square(userInput)
+        
+        print(f"The largest square number less than or equal to {userInput} is: {result}")
+else:
+     print("An error has occured...")
+
+
---
+++ subtask2.py
@@ -0,0 +1,48 @@
+#INF1PR Subtask8.2
+def process_sequence():
+    n = 0
+    s = 0
+    m = 0
+    a = 0
+
+    print("Enter natural numbers one by one (enter -1 to finish)")
+
+    while True:
+        try:
+            # "read x"
+            x_input = input(f"Enter a number (current count {n}): ")
+            x = int(x_input)
+            
+            if x == -1:
+                break
+            
+            if n == 0:
+
+                m = x
+            else:
+
+                if x < m:
+                    m = x
+            
+
+            s = s + x
+            n = n + 1
+            
+        except ValueError:
+            print("Please enter a valid integer.")
+
+    if n == 0:
+        m = -1
+        a = -1
+    else:
+
+        a = s / n
+
+    print("-" * 20)
+    print(f"Count (n): {n}")
+    print(f"Sum (s):   {s}")
+    print(f"Min (m):   {m}")
+    print(f"Mean (a):  {a}")
+
+if __name__ == "__main__":
+    process_sequence()
